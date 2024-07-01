056a5dddd01e112ebe93cde446b071084076de04
adara-code <wakariuki.agnes@gmail.com>
2024-07-02 01:21:11 +0300

    Created a function which uses an external api to return values on current location, city and IP address

---
+++ README.md
@@ -0,0 +1,5 @@
+## Stage One - Weather API
+This project uses Flask to develop a backend server that uses WeatherAPI to return values to a client.
+
+### Instructions
+Add your query parameters on the url as follows ('api/hello?visitor_name=?')
---
+++ app.py
@@ -0,0 +1,43 @@
+import requests
+import os
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+
+load_dotenv()
+
+app = Flask(__name__)
+
+
+@app.route('/api/hello', methods=["GET"])
+def home():
+    """
+    This function returns the client's IP address, location, and city after receiving query parameters from the URL.
+    Real-time data is gathered using Weatherapi.
+    """
+    name = request.args.get('visitor_name').strip('"').title()
+    
+    if name.isalpha():
+        api_key = os.getenv("API_KEY")
+        
+        temperature_endpoint = f"https://api.weatherapi.com/v1/current.json?key={api_key}&q=auto:ip"
+        ip_endpoint = f"https://api.weatherapi.com/v1/ip.json?key={api_key}&q=auto:ip"
+        
+        temp_data = requests.get(temperature_endpoint).json()
+        ip_data = requests.get(ip_endpoint).json()
+        
+        ip_address = ip_data["ip"]
+        city = ip_data["city"].title()
+        temp = temp_data["current"]["temp_c"]
+        
+        
+        response = {
+            "client_ip" : f"{ip_address}",
+            "location" : f"{city}",
+            "greeting": f"Hello, {name}!, The temperature is {temp} degrees celsius in {city}",
+        }
+        
+        return jsonify(response)
+    else:
+        return "Only alphabets allowed as values in the url"
+
+    
